c62be6ed092fab81d3fd6ed9d26d4d2f8522608e
thomasvz14 <thomas@vzunderd.nl>
2026-07-15 10:32:58 +0200

    refactor: relocate dev-server and remove unused tools

--- tools/dev-server.py
+++ tools/dev-server.py
@@ -8,7 +8,7 @@ class SPAHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         # Als het pad geen extensie heeft (zoals /lotte), stuur dan index.html terug (net als Vercel)
         if "." not in self.path:
-            self.path = '/index.html'
+            self.path = '../index.html'
         return super().do_GET()
 
 print(f"Start een lokale server met Vercel-achtige routing op http://localhost:{PORT}")
--- tools/test_pb.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('login.html', 'utf8');
-console.log("File loaded");
